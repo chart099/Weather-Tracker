a1fb79f3455da9f9791e45bcad2d6a4cd0054d41
chart099 <122659980+chart099@users.noreply.github.com>
2023-12-16 22:21:17 -0600

    working on getting this thing to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,71 @@
 var weatherAPIKey = "8737753a701727f24ddb366066ef10a6";
-var weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${currentCity}&appid=${weatherAPIKey}`;
+// var weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${currentCity}&appid=${weatherAPIKey}`;
 
 
 document.getElementById("today-date").innerHTML = dayjs().format("MM/DD/YYYY");
-document.getElementById("day-one-date").innerHTML = dayjs().format("MM/DD/YYYY").add(dayjs.duration(1, 'day'));
-document.getElementById("day-two-date").innerHTML = dayjs().format("MM/DD/YYYY").add(dayjs.duration(2, 'day'));
+document.getElementById("day-one-date").innerHTML = dayjs().add(1, 'day').format("MM/DD/YYYY")
+document.getElementById("day-two-date").innerHTML = dayjs().add(2, 'day').format("MM/DD/YYYY")
+document.getElementById("day-three-date").innerHTML = dayjs().add(3, 'day').format("MM/DD/YYYY")
+document.getElementById("day-four-date").innerHTML = dayjs().add(4, 'day').format("MM/DD/YYYY")
+document.getElementById("day-five-date").innerHTML = dayjs().add(5, 'day').format("MM/DD/YYYY")
 
-const citySubmitHandler = function (event) {
-    event.preventDefault();
+
+
+const getWeatherResults = (searchCity) => {
+    searchCity.preventDefault();
+   
+    const weatherUrl=`https://api.openweathermap.org/data/2.5/forecast?q=${searchCity}&appid=${weatherAPIKey}`;
+    fetch (weatherUrl)
+      .then(function (response) {
+        if (!response.ok) {
+          throw response.json();
+        }
   
-    currentCity = document.getElementById("search-bar").val().trim();
-  
-    if (currentCity) {
-        getUserCity(currentCity);
-        
-    } else {
-      alert('Please enter a City');
+        return response.json();
+      })
+      .then(function (cityInput){
+        document.getElementById("search-bar").textContent =  cityInput.search.query;
+
+        if(!cityInput.results.length) {
+          console.log("No data found")
+          document.getElementById('display-area').innerHTML = "<h1>No weather data found</h1>"
+        } else {
+          for (let i=0; i< cityInput.results.length; i++) {
+            displayResults(cityInput.results[i])
+          }
+        }
+      })
+      .catch(function(error){
+        console.error(error);
+      })
+
     }
-  };
+
+const displayResults=(result)=> {
+  console.log(result)
+}
 
 
-  const doWeatherApp = (currentCity) => {
-    currentCity.preventDefault();
+
+
+  function handleWeatherFormSubmit(event){
+    event.preventDefault();
+
     currentCity = document.getElementById("search-bar").val().trim();
 
-    const response = fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${currentCity}&appid=${weatherAPIKey}`);
-    const data = response.json();
-
-
-
+    if (!searchCity) {
+      alert("You need to input a city to search for weather!")
+      return
+    }
+    getWeatherResults(searchCity);
   }
 
-  doWeatherApp();
+const cityForm = document.querySelector("#city-search-form")
+cityForm.addEventListener('submit', handleWeatherFormSubmit);
+
+
+
+
 
 
 
